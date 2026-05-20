fa6a8389e2df09e729304aff8923a27c963d587c
Global Gamerz <s42485425@gmail.com>
2026-05-20 16:30:09 +0530

    Update download.php

--- download.php
+++ download.php
@@ -1,10 +1,10 @@
 <?php
 // download.php - Place this in your project root
-$file = 'your-app.apk';  // Path to your APK file
+$file = 'JARVIS.apk'; // Path to your APK file
 
 if (file_exists($file)) {
     header('Content-Type: application/vnd.android.package-archive');
-    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Content-Disposition: attachment; filename="' . basename($file) . '"');
     header('Content-Length: ' . filesize($file));
     readfile($file);
     exit;
