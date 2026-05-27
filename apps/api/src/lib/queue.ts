125697838c5059065877fd29746bc754bfed24ca
Global Gamerz <s42485425@gmail.com>
2026-05-27 16:23:16 +0530

    Update import path for env configuration

--- apps/api/src/lib/queue.ts
+++ apps/api/src/lib/queue.ts
@@ -1,6 +1,6 @@
 import { Queue } from "bullmq";
 import IORedis from "ioredis";
-import { env } from "../config/env";
+import { env } from "../config/env.js";
 
 const connection = new IORedis(env.redisUrl, { maxRetriesPerRequest: null });
 
